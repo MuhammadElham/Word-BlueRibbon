cbef000068391a00fdd32860c5b18620a80da586
Elham <elhamwaheed777@gmail.com>
2025-01-26 07:23:10 +0500

    Functionalities Done without CSS

---
+++ main.js
@@ -0,0 +1,29 @@
+const inputField = document.getElementById("input-field");
+const outputField = document.getElementById("output-container");
+const buttons = document.querySelectorAll(".btn"); // Select all buttons
+
+// Result Section
+inputField.addEventListener("keyup", () => {
+  outputField.innerHTML = inputField.value;
+});
+
+// Functionalities for buttons
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    if (btn.classList.contains("uppercase")) {
+      outputField.innerHTML = outputField.innerHTML.toUpperCase();
+    } else if (btn.classList.contains("lowercase")) {
+      outputField.innerHTML = outputField.innerHTML.toLowerCase();
+    } else if (btn.classList.contains("capitalize")) {
+      outputField.innerHTML =
+        outputField.innerHTML.charAt(0).toUpperCase() +
+        outputField.innerHTML.slice(1).toLowerCase();
+    } else if (btn.classList.contains("bold")) {
+      outputField.style.fontWeight = "900";
+    } else if (btn.classList.contains("italic")) {
+      outputField.style.fontStyle = "italic";
+    } else if (btn.classList.contains("underline")) {
+      outputField.style.textDecoration = "underline";
+    }
+  });
+});
